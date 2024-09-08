1595d9f083c7c91fb9b668b04e20787c98425153
HitenMalvi04 <malvihiten8842@gmail.com>
2024-09-08 12:29:21 +0530

    "Footer added"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,8 @@
-import React, { useState } from 'react';
-import Navbar from './Navbar';
-import HamburgerIcon from './HamburgerIcon';
+import React from 'react';
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(true); // Default to false
-
-  // Toggle menu open/close
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
-    <header className={`header-style-2 demo ${isMenuOpen ? 'open' : ''}`}>
+    <header className="header-style-2 demo">
       <div className="wide-container-fluid">
         <div className="row">
           <div className="col-xs-2">
@@ -20,8 +11,37 @@ const Header = () => {
             </a>
           </div>
           <div className="col-xs-10 text-right">
-            {/* Navbar */}
-            <Navbar isMenuOpen={isMenuOpen} />
+            {/* Fixed Navbar */}
+            <ul className="header-menu open">
+              <li className="active"><a href="index.html"><span>Home</span></a></li>
+              <li><a href="about.html"><span>About us</span></a></li>
+              <li><a href="service.html"><span>Services</span></a></li>
+              <li>
+                <a href="portfolio.html"><span>Portfolio</span></a>
+                <ul>
+                  <li><a href="portfolio.html">Portfolio #1</a></li>
+                  <li><a href="portfolio_wide.html">Portfolio #2</a></li>
+                  <li><a href="detail.html">Portfolio detail</a></li>
+                </ul>
+              </li>
+              <li className="dropdown-plus">
+                <a href="blog.html"><span>Blog</span></a>
+                <ul>
+                  <li><a href="post.html">Blog post</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="shop.html"><span>Products</span></a>
+                <ul>
+                  <li><a href="shop.html">Products #1</a></li>
+                  <li><a href="shop2.html">Products #2</a></li>
+                  <li><a href="shop3.html">Products #3</a></li>
+                  <li><a href="shopdetail.html">Detail products</a></li>
+                </ul>
+              </li>
+              <li><a href="checkout.html"><span>Checkout</span></a></li>
+              <li><a href="contact.html"><span>Contact us</span></a></li>
+            </ul>
 
             {/* Basket */}
             <div className="basket open-popup" data-rel="1">
@@ -38,9 +58,6 @@ const Header = () => {
                 <span>Log in &#47; Sign up</span>
               </div>
             </div>
-
-            {/* Hamburger icon */}
-            <HamburgerIcon toggleMenu={toggleMenu} />
           </div>
         </div>
       </div>
